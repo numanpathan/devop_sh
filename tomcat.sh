04efc67b58e835f04ccc2bbbae99237a61600d14
numanpathan <numanpathan341@gmail.com>
2026-08-25 09:55:42 +0530

    Set up Apache Tomcat with user roles
    Install Java, download Tomcat, configure users and roles, and start Tomcat.

---
+++ tomcat.sh
@@ -0,0 +1,16 @@
+dnf install java-21-amazon-corretto -y
+
+wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.59/bin/apache-tomcat-10.1.59.tar.gz
+
+tar -zxvf apache-tomcat-10.1.59.tar.gz
+
+sed -i '56a\<role rolename="manager-gui"/>' apache-tomcat-10.1.59/conf/tomcat-users.xml
+sed -i '57a\<role rolename="manager-script"/>' apache-tomcat-10.1.59/conf/tomcat-users.xml
+sed -i '58a\<user username="tomcat" password="root123456" roles="manager-gui,manager-script"/>' apache-tomcat-10.1.59/conf/tomcat-users.xml
+sed -i '59a\</tomcat-users>' apache-tomcat-10.1.59/conf/tomcat-users.xml
+sed -i '56d' apache-tomcat-10.1.59/conf/tomcat-users.xml
+
+sed -i '21d' apache-tomcat-10.1.59/webapps/manager/META-INF/context.xml
+sed -i '22d' apache-tomcat-10.1.59/webapps/manager/META-INF/context.xml
+
+sh apache-tomcat-10.1.59/bin/startup.sh
